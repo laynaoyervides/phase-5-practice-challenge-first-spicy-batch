f30769677a2414e05f7f1288f4288eb2f1d43cad
Layna Oyervides <laynaoyervides@gmail.com>
2023-02-23 12:02:23 -0700

    commit tree e6c0a43e42ca78446313cd2ba747489cf554a800

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -5,4 +5,18 @@
 
 def first_spicy_batch(n)
   # your code here
+left = 0
+right = n
+
+while left < right
+  mid = left + (right - left) /2
+    if is_spicy_batch(mid)
+      right = mid
+    else
+      left = mid + 1
+    end
+  end
+
+  left
+
 end
